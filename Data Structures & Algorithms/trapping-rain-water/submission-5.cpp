8489341365ca480770c4ef3811d2d2d184f7748e
Alugeli <166392265+Malugeli@users.noreply.github.com>
2026-07-28 17:25:14 +0200

    Add: trapping-rain-water - submission-5

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-5.cpp
@@ -0,0 +1,24 @@
+class Solution{
+    public:
+        int trap(std::vector<int>& height){
+            int left = 0;
+            int right = static_cast<int>(height.size()) -1;
+            int left_max{};
+            int right_max{};
+            int water{};
+
+            while(left < right){
+                left_max = std::max(height[left], left_max);
+                right_max = std::max(height[right], right_max);
+                if(left_max <= right_max){
+                  water += left_max - height[left];
+                  left++;
+                }
+                else{
+                  water += right_max - height[right];
+                  right--;
+                }
+            }
+            return water;
+        }
+};
