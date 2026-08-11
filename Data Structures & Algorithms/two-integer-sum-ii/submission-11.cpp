880a0c26bc562ca957015f34cd1fba68b3ce612a
Alugeli <166392265+Malugeli@users.noreply.github.com>
2026-08-11 09:47:27 +0200

    Add: two-integer-sum-ii - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-11.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& numbers, int target) {
+       int left = 0;
+       int right = static_cast<int>( numbers.size() - 1 ); 
+
+       while(left < right){
+        int sum = numbers[left] + numbers[right];
+        if(sum > target){
+            --right;
+        }
+        else if(sum < target){
+            ++left;
+        } 
+        else{return { left + 1, right + 1 };}
+       }
+       return{};
+    }
+};
