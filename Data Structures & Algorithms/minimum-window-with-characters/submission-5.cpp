5390fce9bbb1164f82ec7104d2a888e83b79ac4b
Alugeli <166392265+Malugeli@users.noreply.github.com>
2026-08-24 11:36:01 +0200

    Add: minimum-window-with-characters - submission-5

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-5.cpp
@@ -0,0 +1,42 @@
+class Solution{
+    public:
+    std::string minWindow(std::string& s, std::string t){
+        if(t.size() > s.size()){
+            return "";
+        }
+        
+        std::array<int, 123> need{};
+        for(auto c : t){
+            ++need[c];
+        }
+        
+        auto best_len = std::string::npos; //"unendlich"
+        size_t best_start{};
+        auto missing = t.size();
+        size_t left{}; 
+
+        for(size_t right = 0; right < s.size(); ++right){
+            char c = s[right];
+            if(need[c] > 0){
+                --missing;
+            }
+            --need[c];
+            while(missing == 0){
+               c = s[left];
+               ++need[c];
+               size_t len = right - left +1;
+               if(len < best_len){
+                best_len = len;
+                best_start = left;
+               }
+               if(need[c] > 0){
+                   ++missing;
+                }
+                left++;
+            }
+        }
+        
+        return best_len == std::string::npos ? "" : s.substr(best_start, best_len);
+
+    }
+};
