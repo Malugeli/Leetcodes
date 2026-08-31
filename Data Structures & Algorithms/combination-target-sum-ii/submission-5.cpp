26c996176f07552336d973804a8d39ebafd4f848
Alugeli <166392265+Malugeli@users.noreply.github.com>
2026-08-31 12:09:31 +0200

    Add: combination-target-sum-ii - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-5.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+  std::vector<std::vector<int>> combinationSum2(std::vector<int> &candidates,
+                                                int target) {
+    std::vector<std::vector<int>> result;
+    std::vector<int> current;
+    std::ranges::sort(candidates);
+    backtracking(result, current, candidates, target, 0);
+    return result;
+  }
+
+private:
+void backtracking(
+    std::vector<std::vector<int>>& result,
+    std::vector<int>& current,
+    std::vector<int>& candidates,
+    int remaining,
+    int start
+){
+    //Basisfall zuerst:
+    if(remaining == 0){
+        result.push_back(current);
+        return;
+    }
+
+    for(int i = start; i < static_cast<int>( candidates.size() ); ++i){
+        if(i > start && candidates[i] == candidates[i - 1]){
+            continue;
+        }
+        if(candidates[i] > remaining){
+            break;
+        }
+        current.push_back(candidates[i]);
+
+        backtracking(result, current, candidates, remaining - candidates[i], i + 1);
+
+        current.pop_back();
+    }
+}
+};
