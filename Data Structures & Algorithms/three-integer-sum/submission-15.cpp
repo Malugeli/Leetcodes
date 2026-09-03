706df2956cad6fe731877bb85c60a058e2c2e2da
Alugeli <166392265+Malugeli@users.noreply.github.com>
2026-09-03 11:02:11 +0200

    Add: three-integer-sum - submission-15

---
+++ Data Structures & Algorithms/three-integer-sum/submission-15.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+  std::vector<std::vector<int>> threeSum(std::vector<int> &nums) {
+    std::vector<std::vector<int>> result;
+    std::ranges::sort(nums);
+
+    for(size_t i = 0; i < nums.size() - 2; ++i){
+        if(i != 0 && nums[i] == nums[i - 1]){
+            continue;
+        }
+        if(nums[i] > 0) break; 
+
+        size_t left = i + 1;
+        size_t right = nums.size() - 1;
+
+        if(nums[i] + nums[left] > 0 ) break;
+
+        while(left < right){
+            int sum = nums[i] + nums[left] + nums[right];
+
+            if(sum > 0){
+                --right;
+            }
+            else if(sum < 0){
+                ++left;
+            }
+            else{
+                result.push_back({nums[i], nums[left], nums[right]});
+
+                int left_value = nums[left];
+                int right_value = nums[right];
+
+                while(left < right && nums[left] == left_value){
+                    ++left;
+                }
+                while(left < right && nums[right] == right_value){
+                    --right;
+                }
+            }
+        }
+    }
+    return result;
+
+}
+};
